d55d88fc027ab66b425082c70c2349c462275f86
John Nganga <jnnganga@gmail.com>
2023-04-02 05:13:40 +0000

    Finish custom filters

--- blango/settings.py
+++ blango/settings.py
@@ -71,6 +71,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'blog_extras': 'blog.templatetags.blog_extras',
+            },
         },
     },
 ]
